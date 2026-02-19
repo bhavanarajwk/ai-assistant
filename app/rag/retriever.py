e17b5c70aac056736067907abc771fea1accf43b
bhavanarajwk <bhavana.r@webknot.in>
2026-02-19 18:08:21 +0530

    Integrated RAG into agent pipeline with tool-calling support using qwen2.5:3b

--- app/rag/retriever.py
+++ app/rag/retriever.py
@@ -1,23 +1,20 @@
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
 
 PERSIST_DIR = "data/vectorstore"
 
 def get_retriever():
-    # Initialize embedding model
     embeddings = OllamaEmbeddings(
         model="nomic-embed-text"
     )
 
-    # Load existing Chroma DB
     vectorstore = Chroma(
         persist_directory=PERSIST_DIR,
         embedding_function=embeddings
     )
 
-    # Create retriever
     retriever = vectorstore.as_retriever(
-        search_kwargs={"k": 3}  # return top 3 relevant chunks
+        search_kwargs={"k": 3}
     )
 
     return retriever
